32ce033045e4b9caddbaa2e0c9c97a1254f83e4a
ZoeGerst <81997340+ZoeGerst@users.noreply.github.com>
2023-06-13 13:12:58 -0700

    Add files via upload

--- Project7/loop.bash
+++ Project7/loop.bash
@@ -2,13 +2,17 @@
 #SBATCH -J Project7
 #SBATCH -A cs475-575
 #SBATCH -p classmpitest
-#SBATCH -N 4 # number of nodes
-#SBATCH -n 4 # number of tasks
+#SBATCH -N 8 # number of nodes
+#SBATCH -n 8 # number of tasks
 #SBATCH --constraint=ib
 #SBATCH -o proj07.out
 #SBATCH -e proj07.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=gerstz@oregonstate.edu
 module load openmpi
-mpic++ proj07.cpp -o proj07 -lm
-mpiexec -mca btl self,tcp -np 4 ./proj07
+
+for p in 1 2 4 8
+do
+	mpic++ proj07.cpp -o proj07 -lm
+	mpiexec -mca btl self,tcp -np $p ./proj07
+done
